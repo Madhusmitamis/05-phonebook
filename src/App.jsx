ae534562cca826e386180a7f48ab437d6c8bd182
Madhusmitamis <148965612+Madhusmitamis@users.noreply.github.com>
2024-04-12 10:00:52 +0300

    2.9*: The Phonebook Step 4 done.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import './App.css'
 
 
 const App = () => {
-const [persons, setPersons] = useState([]) 
+const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber]= useState('')
+  const [searchFilter, setSearchFilter] = useState('')
 
 
   const handleNameChange = (event) => {
@@ -14,6 +20,12 @@ const [persons, setPersons] = useState([])
   const handleNumberChange =(event)=>{
     setNewNumber(event.target.value)
   }
+  const handleSearchFilterChange =(event)=>{
+    setSearchFilter(event.target.value)
+  }
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(searchFilter.toLowerCase())
+  )
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -24,7 +36,7 @@ const [persons, setPersons] = useState([])
       name: newName,
       number:newNumber
     };
-    setPersons(persons.concat(personObject));
+    setPersons([...persons,personObject]);
     setNewName('')
     setNewNumber('')
   }
@@ -35,9 +47,13 @@ const alertUser = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchFilter} onChange={handleSearchFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleNameChange} />
+          name:<input value={newName} onChange={handleNameChange} />
         </div>
         <div>number: <input value={newNumber} onChange={handleNumberChange} /></div>
         <div>
@@ -47,7 +63,7 @@ const alertUser = () => {
       <h2>Numbers</h2>
       <div>
         {/* Render the list of persons */}
-        {persons.map((person, index) => (
+        {filteredPersons.map((person, index) => (
           <div key={index}>{person.name} {person.number}</div>
         ))}
       </div>
